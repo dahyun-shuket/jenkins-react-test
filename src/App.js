c90234005761976453ff5dd057881278aa9012f6
dahyun-shuket <dh.yang@shuket.company>
2022-12-07 16:10:54 +0900

    msg 4

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>222222222</p>
+                <p>3333333333</p>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
                 </a>
