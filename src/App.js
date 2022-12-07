5c1ad69a56b959f624630579bbcb2f4b1e2ede09
dahyun-shuket <dh.yang@shuket.company>
2022-12-07 14:59:13 +0900

    msg 변경

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>hahahahahahaoaho</p>
+                <p>1111111111</p>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
                 </a>
