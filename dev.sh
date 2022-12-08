2bf191f499b6d1a0feb0815b234e1a2ec1487073
dahyun-shuket <dh.yang@shuket.company>
2022-12-08 09:35:13 +0900

    msg

--- dev.sh
+++ dev.sh
@@ -1,6 +1,6 @@
 # cp -r /var/lib/jenkins/workspace/react_test /var/www/jenkins.test
 # cp -R `ls .|grep -v .git` /var/www/jenkins.test/react_test
-cp -r /var/lib/jenkins/workspace/react_test/build /var/www/jenkins.test/react_test
+cp -r `ls .|grep -v .git` /var/www/jenkins.test/react_test
 # npm install
 npm start
 # sudo cp -R `ls .|grep -v .git`
